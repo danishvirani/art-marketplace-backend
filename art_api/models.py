fce1231297d70912525938ad41c6b3c36e0377e8
Danish Virani <danishvirani.tech@gmail.com>
2021-08-24 13:07:11 -0400

    updated auth and cart

--- art_api/models.py
+++ art_api/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class User(models.Model):
     username = models.CharField(max_length=75, unique=True)
     password = models.CharField(max_length=1000)
-    cart = ArrayField()
+    cart = models.ArrayField()
 
 class Artist(models.Model):
     name = models.CharField(max_length=32)
